791c723052c233b4bbb7531474b559730d18a18f
Hassan El Abdallah <hassankh613@gmail.com>
2022-12-26 19:48:05 +0200

    Add dummy controllers

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -2,6 +2,43 @@
 
 const listAttendanceController = async (req, res) => {
 
+    const queryParams = req.query()
+
+    const offeredCourseID = queryParams.offered_course
+
+    // Check if offered course id mentioned in the url 
+    if(!offeredCourseID){
+        return res.status(404).send({'Message':'Offered course id should be included'})
+    }
+
+    try{
+
+        if(req.role = 'T'){
+
+            const offeredCourse = await OfferedCourse.getCourse
+
+            // Dummy method to get the attendances related to (offered course)
+            const attendanceList = await Attendance.getAttendance()
+            
+            res.json(attendanceList)
+
+        } else if(req.role = 'P'){
+
+            // Dummy method to get the attendances related to (parent's student)
+            const attendanceList = await Attendance.getAttendance()
+
+            res.json(attendanceList)
+
+        } else {
+
+        }
+
+    }
+    catch(error){
+        console.log(error)
+        return res.status(404).send({'Message':'Something went wrong'})
+    }
+    
 }
 
 const createAttendanceController = async (req, res) => {
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,45 @@
+const User = require('../models/user')
+const PasswordManager = require('../utils/password')
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+
 
 
 const indexController = async (req, res) => {
 
+    return res.json({'Message':'home'})
 }
 
 const loginController = async (req, res) => {
 
+    let body = req.body
+
+    try{
+
+        // Dummy method to get the user by user id
+        let user = await User.getUser()
+
+        // Compare submited password to user's actuall stored password
+        const passwordIsValid = await PasswordManager.comparePassword(body.password, user.password)
+
+        if (passwordIsValid){
+
+            // Generate a jwt token for the user using his data
+            jwt.sign({user}, JWT_SECRET_KEY, {expiresIn: '30m'}, (err, token)=>{
+                return res.json({
+                    token
+                });
+            })
+        } else {
+            return res.status(401).send({'Message':'Password incorrect'})
+        }
+
+    }
+    catch(error){
+        console.log(error)
+        return res.status(401).send({'Message':'Invalid credentials'})
+    }
 }
 
 
